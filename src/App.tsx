378178d9b12b460e30da8bd4be33e9f7328adfff
Mohamed Zaki <87452598+Mohamed-A-Zaki@users.noreply.github.com>
2026-07-08 06:52:58 +0300

    refactor: move App component to main.tsx and remove App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
+import Providers from "@/app/providers";
 import AppRoutes from "@/shared/routes/app-routes";
 import { Notifications } from "@mantine/notifications";
-import Providers from "./providers";
 
 export default function App() {
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import App from "@/app/App";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 
 import "@/shared/localization/i18n";
 import "@/shared/styles";
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>

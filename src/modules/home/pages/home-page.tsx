f41ca297d1e181d8e5a747f09932a2f45ca3e086
Mohamed Zaki <mohamedzaki385@gmail.com>
2026-08-22 10:30:07 +0300

    refactor: remove unused components and clean up HomePage layout

--- src/modules/home/pages/home-page.tsx
+++ src/modules/home/pages/home-page.tsx
@@ -1,5 +1,4 @@
 import { trans } from "@/shared/localization/trans";
-import { Button, Card, MultiSelect, Select } from "@mantine/core";
 import Helmet from "@mongez/react-helmet";
 
 export default function HomePage() {
@@ -10,24 +9,6 @@ export default function HomePage() {
         description="Best deals every day."
         keywords={["electronics", "deals", "shop"]}
       />
-
-      <div className="py-10">
-        <Card>
-          <Select
-            label="Your favorite library"
-            placeholder="Pick value"
-            data={["React", "Angular", "Vue", "Svelte"]}
-          />
-          <MultiSelect
-            label="Your favorite libraries"
-            placeholder="Pick value"
-            data={["React", "Angular", "Vue", "Svelte"]}
-          />
-          <Button mt={10} className="w-fit! ms-auto">test</Button>
-
-          <div>test</div>
-        </Card>
-      </div>
     </>
   );
 }
